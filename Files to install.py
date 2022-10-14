94c004a6064f849faf4da47d270f321a0cb879ef
AGNISH GHOSH <105692119+AGNISHTHECODER4567@users.noreply.github.com>
2022-10-14 21:59:37 +0530

    Update Files to install.py

--- Files to install.py
+++ Files to install.py
@@ -3,3 +3,10 @@ pyaudio
 pyaudio can have problems while installing ... so i will make a tutorial on this channel >
 https://www.youtube.com/channel/UC35o6NCsvm-PysDft_na1yg
 Check out the tutorial in this channel for any problems!
+and
+datetime
+wikipedia
+os
+webbrowser
+smtplib
+thats all 
