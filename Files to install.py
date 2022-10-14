d794e51610eda0bc7e00689ddcc6e18e14c662c0
AGNISH GHOSH <105692119+AGNISHTHECODER4567@users.noreply.github.com>
2022-10-14 21:58:34 +0530

    Create Files to install.py

---
+++ Files to install.py
@@ -0,0 +1,5 @@
+pyttsx3
+pyaudio
+pyaudio can have problems while installing ... so i will make a tutorial on this channel >
+https://www.youtube.com/channel/UC35o6NCsvm-PysDft_na1yg
+Check out the tutorial in this channel for any problems!
